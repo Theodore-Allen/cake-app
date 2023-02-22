09a08e89f4b012929aeb046c34c834c395e35544
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2023-02-22 09:19:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function app()
 {
-  let endpoint = "https://gist.githubusercontent.com/prayagKhanal/8cdd00d762c48b84a911eca2e2eb3449/raw/5c5d62797752116799aacaeeef08ea2d613569e9/cakes.json";
+  let endpoint = "cakes.json";
   let server = await fetch(endpoint);
   let response = await server.json();
  console.log(response)
